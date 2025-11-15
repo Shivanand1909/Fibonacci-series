4f50c25151e985fb80ed448135b4e7199e33d1fa
Shivanand1909 <shivanandnishad32@gmail.com>
2025-11-15 07:32:40 +0530

    Add files via upload

---
+++ fibonacci_series.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+    int n,n1=0,n2=1,i,n3;
+    printf("Enter the value of n:");
+    scanf("%d",&n);
+    printf("Fibonacci series is:%d %d ",n1,n2);
+    for(i=2;i<=n;i++)
+    {
+        n3=n1+n2;
+        printf("%d ",n3);
+        n1=n2;
+        n2=n3;
+    }
+    return 0;
+}
